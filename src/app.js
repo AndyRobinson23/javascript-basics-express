ed19f10c3d730b1d3c4b98ae7102c85e95bc88ee
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-05-01 14:48:31 +0100

    completed all strings tests and half the numbers tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const strings = require('./lib/strings');
+const numbers = require('./lib/numbers');
 
 const app = express();
 
@@ -18,14 +19,26 @@ app.get('/strings/lower/:string', function (req, res) {
   res.sendStatus(200);
 })
 
-app.get('/strings/first-characters/:string', function (req, res) {
+app.get('/strings/first-character/:string', function (req, res) {
   res.send({result: strings.firstCharacter(req.params.string)});
   res.sendStatus(200);
 })
 
 app.get('/strings/first-characters/:string', function (req, res) {
-  res.send({result: strings.firstCharacters(req.params.string, req.query.length)});
+  res.send({result: strings.firstCharacters(req.params.string, 4)});
   res.sendStatus(200);
+})
+
+app.get('/numbers/add/:number1/and/:number2', function (req, res) {
+  const number1 = parseInt(req.params.number1);
+  const number2 = parseInt(req.params.number2);
+  if(!Number.isNaN(number1) && !Number.isNaN(number2)) {
+    console.log('if statement');
+    res.send({result: numbers.add(Number(req.params.number1), Number(req.params.number2))});
+    res.sendStatus(200);
+  } else {
+    res.status(400).send({error: 'Parameters must be valid numbers.'});
+  }
 })
 
 module.exports = app;
