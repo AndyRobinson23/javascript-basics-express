7417bacda54d1ad816b56e4aaada9b64f3db28de
Andrew Robinson <andrew0robinson@yahoo.co.uk>
2021-05-02 14:14:03 +0100

    completed all division tests

--- src/app.js
+++ src/app.js
@@ -64,4 +64,16 @@ app.post('/numbers/multiply', function (req, res) {
   }
 });
 
+app.post('/numbers/divide', function (req, res) {
+  if(req.body.b === 0) {
+    res.status(400).json({error: 'Unable to divide by 0.'});
+  } else if (req.body.a === undefined || req.body.b === undefined) {
+    res.status(400).json({error: 'Parameters "a" and "b" are required.'});
+  } else if (isNaN(req.body.a) || isNaN(req.body.b)) {
+    res.status(400).json({error: 'Parameters "a" and "b" must be valid numbers.'});
+  } else {
+    res.status(200).json({result: numbers.divide(req.body.a, req.body.b)});
+  }
+});
+
 module.exports = app;
